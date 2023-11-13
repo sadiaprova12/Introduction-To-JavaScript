d1172c5c522daccfb7fe6520c3563d5d1652b1a7
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-13 18:04:37 +0600

    added different variable name type

--- variable-name.js
+++ variable-name.js
@@ -1,1 +1,34 @@
+/***
+
+// Good Variable
 var price = 29;
+
+// 1. Variable name can not be any keywords
+ var false = 96;
+ var return = true;
+
+
+ // 2. Variable name has to be in one work. No space
+
+ var my home address = "New Califonia";
+
+ // 3. Variable name can not hav quatation
+
+ var "name" = "Tom Hanks";
+
+ // 4. variable name can not starts with a number but can ends with a number
+  var 99Club = 1964;
+  var club25 = 2025;
+
+  // 5. how to luse long name
+
+  var user-name = "Raj Bappa"; // can not use dash
+
+var user_name = "Raj Bappa"; // can use 
+
+var usercurrenthomeaddress = "hepatic by postpher" //correct but evabe variable likha thik na . but eder k camel case bola hoy
+var user_home_address = "hepatic by postpher" // correct variable . snake case bola hoy. evabe vabiable name likha best.
+var Userhomeaddress = "hepatic by postpher" // pascal case . correct 
+var user home address = "hepatic by postpher"
+
+ */
