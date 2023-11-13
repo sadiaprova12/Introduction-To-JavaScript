a25a3ecc789499bee2c96e22a696225ad164edb4
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-13 14:26:50 +0600

    change math js file

--- math.js
+++ math.js
@@ -1,22 +1,24 @@
 /***
  * 1. number ==> 55, 898, -32
- * 2. integer, odd, even: 2,5,
+ * 2. integer: 2,5, [kunta odd(), kunta even()]
  * 3. float: decimal: 2.5, 13.67, 32.69
  * 4. operation: addition, substraction, multiplication,division,reminder
  * 5. percent
  * 6. average
- * 7. comparison: 2 theke 5 boro tahole ">" chinho.
- * 2 theke 5 choto tahole "<" chinho.
- * jodi 2 jon soman taka ba kisu thake tahole "==" chinho.
- * jodi kuno kisu e shoman na tahole "!=" chinho.
- * boro ba shoman tahole ">=" chinho.
- * choto ba shoman tahole "<=" chinho.
- * 8. oikik niom math: 2 kilo apple and 5 kilo angur proti kilo dam koto hbe? emn typer hisab bujhte hbe
-* 9.  Series
+ * 7. comparison: >, <, ==, !=, >=, <=
+ * 2 theke 5 boro tahole ">" gather than.
+ * 2 theke 5 choto tahole "<" less than.
+ * jodi 2 jon soman taka ba kisu thake tahole "==" .
+ * jodi kuno kisu e shoman na tahole "!=" .
+ * boro ba shoman tahole ">=" .
+ * choto ba shoman tahole "<=" .
+ * 8. oikik niom math: 2 kilo apple and 5 kilo angur proti kilo dam koto hbe? emn typer hisab bujhte hbe.
+ * Dhoro onk gula sonkha ase . Egula comparisom korte hbe . Jmeon 45, 25, 68, 129, 2 : egula moddhe sob cheye boro ba maximum sonkha kunta? Egular moddhe sob cheye choto ba minimum sonkha kunta? egula Average koto? Egular sum koto khuje bher koro ?
+* 9.  Series :
 *:::Extra:::
 *1. Prime
-*2. permutation
-* Area: How to find area of a try angle.
+*2. Permutation
+* Area: How to find area of a try angle?
 *::::Always:::
 *
 */
