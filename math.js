1a94bb962ad99b510bf48eaaf126813561f40ce0
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-13 14:05:18 +0600

    added js file

---
+++ math.js
@@ -0,0 +1,22 @@
+/***
+ * 1. number ==> 55, 898, -32
+ * 2. integer, odd, even: 2,5,
+ * 3. float: decimal: 2.5, 13.67, 32.69
+ * 4. operation: addition, substraction, multiplication,division,reminder
+ * 5. percent
+ * 6. average
+ * 7. comparison: 2 theke 5 boro tahole ">" chinho.
+ * 2 theke 5 choto tahole "<" chinho.
+ * jodi 2 jon soman taka ba kisu thake tahole "==" chinho.
+ * jodi kuno kisu e shoman na tahole "!=" chinho.
+ * boro ba shoman tahole ">=" chinho.
+ * choto ba shoman tahole "<=" chinho.
+ * 8. oikik niom math: 2 kilo apple and 5 kilo angur proti kilo dam koto hbe? emn typer hisab bujhte hbe
+* 9.  Series
+*:::Extra:::
+*1. Prime
+*2. permutation
+* Area: How to find area of a try angle.
+*::::Always:::
+*
+*/
---
+++ variable.js
@@ -0,0 +1,18 @@
+//Variable Declare
+
+//numeric
+ var EggPrice = 120;
+ var Age = 23;
+ var Temperature = 22;
+ var Weight = 58;
+ var Pocketmoney = 200;
+
+ //String
+ var Person = "Sadia Siddika";
+ var Location = "Uttara-Dhaka";
+ var Special = "Samnan";
+
+ //Bollean
+ var Serious = true;
+ var isSingle = false;
+ var isFullMarks = true;
