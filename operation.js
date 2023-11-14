19ebc2749faaf3c7e695b21d2b1202415ef89aa2
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-14 21:55:26 +0600

    each player divison

--- operation.js
+++ operation.js
@@ -19,3 +19,10 @@ var orangePrice = 20;
 var quantity = 7;
 var totalCost = orangePrice * quantity;
 console.log(totalCost); */
+
+// Division
+
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
