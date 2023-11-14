046399b7dadfef5afbeef35aecdddf0160d220ef
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-14 22:53:08 +0600

    added concatination file

---
+++ concatination.js
@@ -0,0 +1,9 @@
+var price1 = 31;
+var price2 = 10;
+var totalPrice = price1 + 10;
+// console.log(totalPrice);
+
+var firstName = 'Tom &';
+var secondName = 'Jerry';
+var fullName = firstName + ' ' + secondName;
+console.log(fullName);
