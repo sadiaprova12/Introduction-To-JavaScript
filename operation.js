ca82d3bac739ee30c50040d7da972f4069dbf836
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-14 21:53:07 +0600

    total cose multiplication

--- operation.js
+++ operation.js
@@ -2,13 +2,20 @@ var onionPrice = 20;
 var eggPrice = 10;
 
 //total proce bher korar process (Addition + )
-var totalPrice = onionPrice + eggPrice;
-console.log(totalPrice);
+/** var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice); */
 /**console.log(onionPrice);
 console.log(eggPrice);
 console.log(onionPrice + eggPrice); */
 
 // 2 ta proce r moddhe difference bher korar process (Subtraction - )
 
-var priceDifference = onionPrice - eggPrice;
-console.log(priceDifference);
+/** var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference); */
+
+/**  Multiplication 
+
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice * quantity;
+console.log(totalCost); */
