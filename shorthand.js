b9741e62e514bca37a1e5eb8b87ec15a7fc804aa
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-14 22:24:37 +0600

    added set a new value and previous value

---
+++ shorthand.js
@@ -0,0 +1,15 @@
+// variable declaration process
+var tomatoPrice = 25;
+// variable value update process
+tomatoPrice = 30;
+
+// add somthing to the previous value [variable maan ta k notun vabe set korbo na borong ager maan r sathy kisu value addition (+) korbo. Sei process hocche:  ]
+var price1 = 31; // price1 er maan notun vabe maan set korte chai,tobe setar ager maan r sathy 10 addition (+) kore
+var price2 = 10;
+// set a new value
+//price1 = 35;
+
+// add some value to the existing value
+price1 = price1 + 10;
+console.log(price1);
+
