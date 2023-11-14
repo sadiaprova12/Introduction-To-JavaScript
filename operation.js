4893ee3f987eb11bd9f5e5f9f30e3ccd7c3395f3
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-14 21:44:08 +0600

    added price difference

--- operation.js
+++ operation.js
@@ -1,9 +1,14 @@
 var onionPrice = 20;
 var eggPrice = 10;
 
+//total proce bher korar process (Addition + )
 var totalPrice = onionPrice + eggPrice;
 console.log(totalPrice);
 /**console.log(onionPrice);
 console.log(eggPrice);
 console.log(onionPrice + eggPrice); */
 
+// 2 ta proce r moddhe difference bher korar process (Subtraction - )
+
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
