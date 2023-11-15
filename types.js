0c1e64a915767a14155cecb5ee61d886e3d6a9b2
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-15 19:18:58 +0600

    added to fixed process

--- types.js
+++ types.js
@@ -6,7 +6,14 @@ var price = '100';
 //console.log(typeof price); // string types
 
 var isHappy = true;
-console.log(typeof isHappy); // boolean type
+//console.log(typeof isHappy); // boolean type
 
 var unSocail;
-console.log(unSocail);
+//console.log(unSocail); //undefinied
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(1); // toFixed deyar reason just 0.1 and 0.2 r khetre sob sommoy dhoshomik r por 8,9 tar maan besi digit ase .  tar jonno toFixed(2) deya hoise dhoromik r por jeno 2 ta digit show kore .
+sum = parseFloat(sum); // purnno shonkha anar jonno
+console.log(sum);
