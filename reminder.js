2c1d7bcb1aa0fd074bb6d6aff566e0596775817d
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-15 20:00:14 +0600

    added task 1

--- reminder.js
+++ reminder.js
@@ -3,4 +3,18 @@ var person = 2;
 // [/] this means vagfol
 // [%] this means vagsesh
 var reminder = mangos % person;
-console.log(reminder);
+//console.log(reminder);
+
+/** Task 1 */
+var harryMom = 1000;
+//console.log(harryMom);
+
+var apple = 400;
+var orange = 300;
+var totalFruitPrice = 400 + 300;
+//console.log(totalFruitPrice);
+
+var harryMom = 1000;
+var totalFruitPrice = 700;
+var saveMoney = harryMom - totalFruitPrice;
+console.log(saveMoney);
