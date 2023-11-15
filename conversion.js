319974d4b41b785f5838a84921e6e6ce55c98a46
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-15 18:45:42 +0600

    added parse float process

--- conversion.js
+++ conversion.js
@@ -8,3 +8,10 @@ console.log(price1);
 // kuno ekta jinish k convert korar process [parse]
 var price1Number = parseInt(price1);
 console.log(price1Number);
+
+//
+var gpa ='3.41';
+var gpaNumber = parseInt(gpa); // parseInteger dile doshomik r porer maan ase na .
+// Doshomik shoho maan anar jonno parseFloat dite hbe.
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
