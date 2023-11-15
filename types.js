051228a797786bc1363702479249ec454ffa5b96
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-15 19:09:32 +0600

    added different typesof

--- types.js
+++ types.js
@@ -1,3 +1,12 @@
 var price = 100;
 //console.log(price);
-console.log(typeof price); // typeof deyar reason [price er j variable ase sei variable ta ki dhoroner maan ase setar bujhar jonno]
+//console.log(typeof price); // typeof deyar reason [price er j variable ase sei variable ta ki dhoroner maan ase setar bujhar jonno. answer: number type]
+
+var price = '100';
+//console.log(typeof price); // string types
+
+var isHappy = true;
+console.log(typeof isHappy); // boolean type
+
+var unSocail;
+console.log(unSocail);
