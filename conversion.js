c732de0a4795575622d1abaa1a748b23abbffcd4
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-15 18:40:21 +0600

    added parse proccess

--- conversion.js
+++ conversion.js
@@ -1,9 +1,10 @@
 var price1 = '31';
-var price2 = '10';
+var price2 = 10;
 var totalPrice = price1 + price2;
  console.log(totalPrice);
 
-/* var firstName = 'Tom &';
-var secondName = 'Jerry';
-var fullName = firstName + ' ' + secondName;
-console.log(fullName); */
+// convert string to number 
+console.log(price1);
+// kuno ekta jinish k convert korar process [parse]
+var price1Number = parseInt(price1);
+console.log(price1Number);
